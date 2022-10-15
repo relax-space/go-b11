1593946ccf2f32f4b9a203695621db972369f93b
xiaoxinmiao <xiaoxm_001@163.com>
2022-10-15 19:15:45 +0800

    v1.0.0

---
+++ b11/b11.go
@@ -0,0 +1,7 @@
+package b11
+
+import "github.com/relax-space/go-a11/a11"
+
+func Hello() string {
+	return a11.Hello()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/relax-space/go-b11
+
+go 1.19
+
+require github.com/relax-space/go-a11 v1.0.2
